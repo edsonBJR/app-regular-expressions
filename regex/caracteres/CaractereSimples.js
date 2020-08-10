e66b7b23dea0db543764d36d5eb6d9742740be25
edsonBJR <ebjr.tux@gmail.com>
2020-08-09 21:46:44 -0300

    Busca Caracteres Simples

--- regex/caracteres/CaractereSimples.js
+++ regex/caracteres/CaractereSimples.js
@@ -1,4 +1,17 @@
 const texto = '1,2,3,4,5,6,a.b c!d?e'
 
 const regexVirgula = /,/
+    // Vai quebar o texto passando o padrão de expressão regular passado e colocando dentro de um array
 console.log(texto.split(regexVirgula))
+
+// Vai mostrar a primeira ocorrencia do padrão de expressão regular passado
+console.log(texto.match(regexVirgula))
+
+console.log(texto.match(/,/g))
+console.log(texto.match(/A/))
+
+// Com a flag i ele não faz distinção de Maiusculas e Minusculas
+console.log(texto.match(/A/gi))
+
+console.log(texto.match(/2/g))
+console.log(texto.match(/b c!d/))
--- regex/executandoRegex/UsandoJava.java
+++ regex/executandoRegex/UsandoJava.java
@@ -2,7 +2,7 @@ import java.util.regex.*;
 
 public class UsandoJava {
     public static void main(String[] args){
-        String texto = "0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f";q
+        String texto = "0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f";
         Pattern  patternLetras = Pattern.compile("[a-f]");
         Matcher matcher = patternLetras.matcher(texto);
 
